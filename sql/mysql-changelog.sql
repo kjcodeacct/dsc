61d9d4ef0a53a579eb20bc4eb114f0a558b00d0b
kjcodeacct <kjcodeacct@gmail.com>
2019-10-08 07:34:09 -0500

    added temporal log data to mysql and sqlite sql

--- sql/mysql-changelog.sql
+++ sql/mysql-changelog.sql
@@ -2,17 +2,42 @@ CREATE TABLE dsc_changelog
 (
 	id SERIAL PRIMARY KEY,
 	sha256 TEXT NOT NULL,
-	epoch INTEGER NOT NULL,
+	-- created at = time commited
+	created_at INTEGER NOT NULL,
+	-- applied at = time sql is ran on remote 
+	applied_at INTEGER,
+	-- completed at = time sql is completed and transaction applied
+	completed_at INTEGER,
 	author_name TEXT NOT NULL,
 	author_email TEXT NOT NULL
 );
 
-CREATE TABLE dsc_change_files
+CREATE TABLE dsc_change_file
 (
 	id SERIAL PRIMARY KEY,
-	changelog_id INTEGER NOT NULL REFERENCES dsc_changelog(id) ON DELETE CASCADE,
-	file TEXT NOT NULL
+	file TEXT NOT NULL,
+	created_at INTEGER NOT NULL
 );
+
+CREATE UNIQUE INDEX ON dsc_change_files(file);
+
+CREATE TABLE dsc_temporal_log
+(
+	id SERIAL PRIMARY KEY,
+	dsc_change_file_id INTEGER NOT NULL REFERENCES dsc_change_file(id),
+	updated_at INTEGER NOT NULL,
+	change_type TEXT NOT NULL REFERENCES dsc_change_types(type),
+	author_name TEXT NOT NULL,
+	author_email TEXT NOT NULL
+);
+
+CREATE TABLE dsc_change_types
+(
+	id SERIAL PRIMARY KEY,
+	type TEXT NOT NULL,
+);
+
+CREATE UNIQUE INDEX ON dsc_change_types(type);
 
 CREATE TABLE dsc_config
 (
--- sql/sqlite-changelog.sql
+++ sql/sqlite-changelog.sql
@@ -2,18 +2,42 @@ CREATE TABLE dsc_changelog
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	sha256 TEXT NOT NULL,
-	epoch INTEGER NOT NULL,
-	applied_at_epoch INTEGER,
+	-- created at = time commited
+	created_at INTEGER NOT NULL,
+	-- applied at = time sql is ran on remote 
+	applied_at INTEGER,
+	-- completed at = time sql is completed and transaction applied
+	completed_at INTEGER,
 	author_name TEXT NOT NULL,
 	author_email TEXT NOT NULL
 );
 
-CREATE TABLE dsc_change_files
+CREATE TABLE dsc_change_file
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	changelog_id INTEGER NOT NULL REFERENCES dsc_changelog(id) ON DELETE CASCADE,
-	file TEXT NOT NULL
+	file TEXT NOT NULL,
+	created_at INTEGER NOT NULL
 );
+
+CREATE UNIQUE INDEX ON dsc_change_files(file);
+
+CREATE TABLE dsc_temporal_log
+(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	dsc_change_file_id INTEGER NOT NULL REFERENCES dsc_change_file(id),
+	updated_at INTEGER NOT NULL,
+	change_type TEXT NOT NULL REFERENCES dsc_change_types(type),
+	author_name TEXT NOT NULL,
+	author_email TEXT NOT NULL
+);
+
+CREATE TABLE dsc_change_types
+(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	type TEXT NOT NULL,
+);
+
+CREATE UNIQUE INDEX ON dsc_change_types(type);
 
 CREATE TABLE dsc_config
 (
