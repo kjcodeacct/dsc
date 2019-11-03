98e7be7f9a80427502d21049d4f277c5012b0479
kjcodeacct <kjcodeacct@gmail.com>
2019-11-03 15:36:16 -0600

    refactored parser and added test, completed corner cases for sql parsing function

--- src/sql/parse.go
+++ src/sql/parse.go
@@ -2,54 +2,151 @@ package sql
 
 import (
 	"bufio"
-	"fmt"
+	"dsc/fancy_errors"
 	"strings"
-	"time"
+	"unicode"
 )
 
-type QueryGroup struct {
-	Priority   int
-	CommitHash string
-	List       []Query
-}
+/*
+Query
 
+The query object contains the metadata used for executing and logging a query ran via DSC.
+*/
 type Query struct {
-	Priority  int
-	Filename  string
+	Content   string
 	LineStart int
 	LineEnd   int
-	StartTime time.Time
-	EndTime   time.Time
-	Content   string
 }
 
 /*
-Cases:
+Parse
 
-insert into....; select from ...;
+This function parses text input and splits them into 'Query' objects containing the query's content,
+and what line it starts and ends on. This is used for processing and logging by the sql
+execution functions.
 
+Potential cases for parsing query 'N':
 
+1. Single line with 1 query:
+'''
+N;
+'''
 
+2. Single line with multiple queries. (N1; N2; N3; N4):
+'''
+N1; N2; N3; N4;
+'''
+
+3. Single line with multiple queries AND a multi line query(s):
+N1; N2; N3; N4a
+N4b
+N4c;
+
+4. Multi line query:
+Na
+Nb
+Nc
+Nd;
+
+5. Multi line query AND single line query(s):
+N1a
+N1b
+N1c
+N1d;N2;N3;
 */
-
-func Parse(input string) (QueryGroup, error) {
-	var newQueryGroup QueryGroup
+func Parse(input string) ([]Query, error) {
 
 	scanner := bufio.NewScanner(strings.NewReader(input))
 
 	lineCount := 0
-	var newQuery Query
-	for scanner.Scan() {
-		lineCount++
-		lineStr := scanner.Text()
 
-		if strings.Contains(lineStr, ";") {
-			// detect multiple splits
-			newQuery.LineEnd = lineCount
+	var queryList []Query
+
+	query := &Query{}
+
+	for scanner.Scan() {
+
+		lineCount++
+
+		lineString := scanner.Text()
+
+		lineString = tabToSpace(lineString)
+
+		if strings.Contains(lineString, ";") {
+
+			// account for case 2, 3, & 5
+			if strings.Count(lineString, ";") > 1 {
+				lineQueryList := strings.SplitAfter(lineString, ";")
+
+				for i, lineQuery := range lineQueryList {
+					if i == 0 {
+						query.Content += lineQuery + "\n"
+						query.LineEnd = lineCount
+
+						queryList = append(queryList, *query)
+
+						query = &Query{}
+					} else {
+
+						if strings.Contains(lineQuery, ";") {
+							query.Content += lineQuery + "\n"
+							query.LineEnd = lineCount
+
+							queryList = append(queryList, *query)
+
+							query = &Query{}
+						} else {
+							query.Content += lineQuery + "\n"
+						}
+					}
+				}
+
+			} else {
+				query.Content += lineString + "\n"
+				query.LineEnd = lineCount
+
+				queryList = append(queryList, *query)
+
+				query = &Query{}
+			}
+
+		} else if strings.ReplaceAll(lineString, " ", "") != "\n" {
+
+			// account for case 1, 3, 4, &5
+			if query.LineStart == 0 {
+				query.LineStart = lineCount
+			}
+
+			query.Content += lineString + "\n"
+
 		}
 	}
 
-	if lineCount == 1 && len(newQueryGroup.List) > 1 {
-		fmt.Println("Why did you single line this query?\nWe're compressing it before storage man, it'll be ok...")
+	if len(queryList) == 0 {
+		return nil, fancy_errors.New("no valid queries present in input")
 	}
+
+	return queryList, nil
+}
+
+/*
+tabToSpace
+
+This function standardizes the input for easier parsing by stripping tabs and replacing them
+with spaces.
+*/
+func tabToSpace(input string) string {
+	var result []string
+
+	for _, i := range input {
+		switch {
+		// all these considered as space, including tab \t
+		// '\t', '\n', '\v', '\f', '\r',' ', 0x85, 0xA0
+		case unicode.IsSpace(i):
+			result = append(result, " ") // replace tab with space
+		case !unicode.IsSpace(i):
+			result = append(result, string(i))
+		}
+	}
+	return strings.Join(result, "")
 }
---
+++ src/sql/parse_test.go
@@ -0,0 +1,48 @@
+package sql
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testOne = `
+SELECT *
+FROM test
+WHERE test_a IS NOT NULL;
+`
+
+const testTwo = `
+INSERT INTO test(a,b)VALUES(1,2);INSERT INTO test_other(b,c)VALUES(1,2);INSERT INTO test_other(b,c)
+VALUES(1,2);
+
+SELECT *
+FROM test
+WHERE text_d IS NOT NULL;
+`
+
+func TestParser(t *testing.T) {
+
+	queryList, err := ParseDeus(testOne)
+
+	for _, query := range queryList {
+		fmt.Println("Query:", query.Content)
+		fmt.Println("LineStart:", query.LineStart)
+		fmt.Println("LineEnd:", query.LineEnd)
+		fmt.Println("")
+	}
+
+	require.Nil(t, err)
+
+	queryList, err = ParseDeus(testTwo)
+
+	for _, query := range queryList {
+		fmt.Println("Query:", query.Content)
+		fmt.Println("----LineStart:", query.LineStart)
+		fmt.Println("----LineEnd:", query.LineEnd)
+		fmt.Println("")
+	}
+
+	require.Nil(t, err)
+}
